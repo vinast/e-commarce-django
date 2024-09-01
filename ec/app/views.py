360d9d9e17a113f40d296f475686f12660742b1e
vinast <dvvinas2@gmail.com>
2024-09-02 03:03:29 +0700

    Commit membuat halaman about dan contact untuk product done

--- ec/app/views.py
+++ ec/app/views.py
@@ -1,5 +1,4 @@
 from django.db.models import Count
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from . models import Product
@@ -8,9 +7,29 @@ from . models import Product
 def home(request):
     return render(request, "app/home.html")
 
+def about(request):
+    return render(request, "app/about.html")
+
+def contact(request):
+    return render(request, "app/contact.html")
+
 
 class CategoryView(View):
     def get(self, request, val):
         product = Product.objects.filter(category=val)
         title = Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
         return render(request, "app/category.html", locals())
+    
+
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request, "app/category.html", locals())
+    
+
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, "app/productdetail.html", locals())
